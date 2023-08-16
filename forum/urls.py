b59a2ca1fcc8a2f130a0df1b03a74b708d7c1455
John McGrath <johnpmcgrath1996@gmail.com>
2023-08-16 12:27:44 +0000

    update readme

--- forum/urls.py
+++ forum/urls.py
@@ -3,7 +3,9 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
+    path('create/', views.create_post, name='create_post'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('upvote/<slug:slug>', views.PostUpvote.as_view(), name='post_upvote'),
     path('downvote/<slug:slug>', views.PostDownvote.as_view(), name='post_downvote'),
+    
 ]
